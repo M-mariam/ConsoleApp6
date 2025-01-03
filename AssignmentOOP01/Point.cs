d744e1e355a80086f3b4c26a490ec110bb17d5e6
m-mariam <mariamabdelaziz622@gmail.com>
2025-01-03 19:46:28 +0200

    assignment 1 OOP

--- AssignmentOOP01/Point.cs
+++ AssignmentOOP01/Point.cs
@@ -8,5 +8,12 @@ namespace AssignmentOOP01
 {
     internal struct Point
     {
+        public double X;
+        public double Y;
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
